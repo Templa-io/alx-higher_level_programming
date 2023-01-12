3aac988bf8df1ed00e5cc930b019cbb982b8e01f
Templa-io <humphreywritter@gmail.com>
2023-01-12 11:06:12 -0800

    update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x*number, my_list))
-
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
