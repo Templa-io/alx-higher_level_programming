8193203f409b44b4a7735cb765a9ce86f1d67999
Templa-io <humphreywritter@gmail.com>
2023-01-12 12:06:38 -0800

    update list file

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to the head of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t count = 0;
+    const dlistint_t *current = h;
+
+    while (current)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+        count++;
+    }
+
+    return count;
+}
