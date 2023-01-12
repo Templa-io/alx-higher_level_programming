3d37a6fdb898846d647a33d5784cb0425c2b47da
Templa-io <humphreywritter@gmail.com>
2023-01-12 11:37:24 -0800

    update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+
 def complex_delete(a_dictionary, value):
-    keys_to_delete = [k for k, v in a_dictionary.items() if v == value]
-    for k in keys_to_delete:
-        del a_dictionary[k]
+    """Delete keys with a specific value in a dictionary."""
+    while value in a_dictionary.values():
+        for k, v in a_dictionary.items():
+            if v == value:
+                del a_dictionary[k]
+                break
+
+    return (a_dictionary)
