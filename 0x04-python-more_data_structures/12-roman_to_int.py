ce1954c332adfe5ed37c4327c662427d845e525b
Templa-io <humphreywritter@gmail.com>
2023-01-12 12:57:28 -0800

    update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,30 @@
 
 
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
-        return 0
-    roman_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    result = 0
+    """Converts a roman numeral to an integer."""
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
+        return (0)
+
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
+    num = 0
+
     for i in range(len(roman_string)):
-        if i > 0 and roman_map[roman_string[i]] > roman_map[roman_string[i - 1]]:
-            result += roman_map[roman_string[i]] - 2 * roman_map[roman_string[i - 1]]
+        if roman_dict.get(roman_string[i], 0) == 0:
+            return (0)
+
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+                num += roman_dict[roman_string[i]] * -1
+
         else:
-            result += roman_map[roman_string[i]]
-    return result
+            num += roman_dict[roman_string[i]]
+    return (num)
