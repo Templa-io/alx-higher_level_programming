c1e7bc48754a6c2c4f1dbe133216b000e838df35
Templa-io <humphreywritter@gmail.com>
2023-01-12 11:40:17 -0800

    update 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    return list(map(lambda sub_list: list(map(lambda x: x*x, sub_list)), matrix))
+    return (list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix)))
