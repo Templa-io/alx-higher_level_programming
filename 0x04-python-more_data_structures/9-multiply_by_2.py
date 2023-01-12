f16de74c83c31c9e3658cfed7565d0513821efbf
Templa-io <humphreywritter@gmail.com>
2023-01-12 10:52:52 -0800

    update 8-simple_delete.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
-def multiply_by_2(a_dictionary):
-    return {key: value*2 for key, value in a_dictionary.items()}
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return (a_dictionary)
