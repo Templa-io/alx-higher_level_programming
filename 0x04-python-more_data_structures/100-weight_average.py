181b0fc3108b09c12a757ef8fdd25fa3a1af76f4
Templa-io <humphreywritter@gmail.com>
2023-01-12 11:20:27 -0800

    update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,4 +6,4 @@ def weight_average(my_list=[]):
         return 0
     total_weight = sum(weight for score, weight in my_list)
     weighted_sum = sum(score * weight for score, weight in my_list)
-    return weighted_sum / total_weighti
+    return weighted_sum / total_weight
