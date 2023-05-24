b4c56639b190299740012774d3f51677414e5258
Templa-io <humphreywritter@gmail.com>
2023-05-24 10:30:56 +0000

    update new files

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-fs.readFile("cisfun.text", 'utf-8', function (err, result) {
+fs.readFile(process.argv[2], 'utf-8', function (err, result) {
   if (err) {
     console.log(err);
   } else {
